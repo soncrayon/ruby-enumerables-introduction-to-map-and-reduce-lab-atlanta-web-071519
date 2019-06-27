3c1bd8ade5fe05114c0758f48c14ffe626dd3a0b
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-27 19:11:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,13 +54,10 @@ def reduce_to_total(source_array, *starting_point)
 end 
 
 def reduce_to_all_true(source_array)
-  i = 0 
-  for i in source_array  
-    if i 
+  if source_array.all?
       return true  
     else
-      break 
-    end
-  return false 
+      return false 
+  end 
 end 
 
